1f97095a3105a12e02acbe03e10541449e93fe63
Mar1nbawe <vladcostinmc@yahoo.ro>
2024-11-17 17:54:01 +0200

    modified LoggingMiddleware
    Current Middleware will provide more information about accesses to displayed routes.

--- Router/Middleware.go
+++ Router/Middleware.go
@@ -1,15 +1,63 @@
 package router
 
 import (
-	"fmt"
 	"net/http"
+	"runtime/debug"
+	"time"
+
+	log "github.com/go-kit/kit/log"
 )
 
-func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+type Middleware func(http.HandlerFunc) http.HandlerFunc
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request received:", r.Method, "in link:", r.URL.Path, "\n from IP:", r.RemoteAddr)
-		next.ServeHTTP(w, r)
+func LoggingMiddleware(logger log.Logger) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					logger.Log(
+						"err", err,
+						"trace", debug.Stack(),
+					)
+				}
+			}()
+
+			start := time.Now()
+			wrapped := wrapResponseWriter(w)
+			next.ServeHTTP(wrapped, r)
+			logger.Log("status", wrapped.status,
+				"method", r.Method,
+				"path", r.URL.EscapedPath(),
+				"duration", time.Since(start),
+			)
+		}
+
+		return http.HandlerFunc(fn)
 	}
-
 }
+
+//func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+//
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		fmt.Println("Request received:", r.Method, "in link:", r.URL.Path, "\n from IP:", r.RemoteAddr)
+//		next.ServeHTTP(w, r)
+//	}
+//
+//}
+
+//func MethodNotAllowedMiddleware(next http.HandlerFunc, allowedMethods string) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		methodallowed := false
+//		for _, method := range allowedMethods {
+//			if r.Method == method {
+//				methodallowed = true
+//				break
+//			}
+//		}
+//		if !methodallowed {
+//			http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
+//		}
+//		next.ServeHTTP(w, r)
+//	}
+//}
--- index.go
+++ index.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-kit/kit/log"
+	stdlog "log"
 	"net/http"
+	"os"
 
 	router "Web-Router/Router"
 )
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("test"))
+	message := "Default message"
+	if code, ok := r.Context().Value("code").(int); ok {
+		message = router.MessageCodes[code]
+	}
+	w.Write([]byte(message))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	message := "This is the default page"
+
+	w.Write([]byte(message))
 }
 
 func main() {
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	stdlog.SetOutput(log.NewStdlibAdapter(logger))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "loc", log.DefaultCaller)
 
 	r := &router.Router{}
 
-	r.Use(router.LoggingMiddleware)
-	r.Route(http.MethodGet, "/about", aboutHandler)
-	http.ListenAndServe(":8000", r)
-	fmt.Println("Listening to inputs!")
+	r.Use(router.LoggingMiddleware(logger))
+	r.Route(http.MethodGet, "/home", homeHandler, 1)
+	r.Route(http.MethodGet, "/about", aboutHandler, 0)
+	http.ListenAndServe("localhost:8000", r)
+
 }
